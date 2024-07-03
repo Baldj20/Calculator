7ab0f412d1eae05cb3a4ad0e41f76af087f11214
Evgenyi <evgenyisavizki@gmail.com>
2024-07-03 18:14:50 +0300

    Implemented variable logic

--- Variables.cs
+++ Variables.cs
@@ -6,7 +6,39 @@ using System.Threading.Tasks;
 
 namespace Calculator
 {
-    internal class Variables
+    internal static class Variables
     {
+        private static Dictionary<string, string> variables = new Dictionary<string, string>();
+
+        public static string GetVariableValue(string variableName)
+        {
+            if (IsVariableDefined(variableName))
+            {
+                return variables[variableName];
+            }
+            else
+            {
+                throw new Exception($"Variable '{variableName}' is not defined.");
+            }
+        }
+
+        public static void SetVariableValue(string variableName, string variableValue)
+        {
+            variables[variableName] = variableValue;
+        }
+
+        public static bool IsVariableDefined(string variableName)
+        {
+            return variables.ContainsKey(variableName);
+        }
+
+        public static string ReplaceVariables(string expression)
+        {
+            foreach (var variable in variables)
+            {
+                expression = expression.Replace(variable.Key, variable.Value.ToString());
+            }
+            return expression;
+        }
     }
 }
