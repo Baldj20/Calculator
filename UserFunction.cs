cd15b46d391ac718bee8e04677f0b06c3bc3c917
Олег Пьявко <pyavko12345678@gmail.com>
2024-07-04 20:32:12 +0300

    Удалены все комментарии

--- UserFunction.cs
+++ UserFunction.cs
@@ -9,13 +9,13 @@ namespace Calculator
 {
     internal class UserFunction
     {
-        private static readonly List<char> invalidCharacters = new List<char> //Недопустимые символы
+        private static readonly List<char> invalidCharacters = new List<char> 
         {
             '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '+', '=', '{', '}', '[', ']', '|', '\\', ':', ';', '"', '\'', '<', '>', ',', '.', '?', '/', ' ', '\t', '\n', '\r'
         };
-        private string _name; // Название функции
-        private string[] _functionArgs; // Переменные функции
-        private string _body; // Тело функции
+        private string _name; 
+        private string[] _functionArgs;
+        private string _body;
         public UserFunction(string name, string[] functionArgs, string body)
         {
             Name = name;
@@ -44,34 +44,34 @@ namespace Calculator
         }
 
 
-        static bool isFunctionCreating(string exeption) // Метод будет вызываться для проверки создается ли функция 
+        static bool isFunctionCreating(string exeption)
         {
             if (exeption.Contains("="))
             {
                 var parts = exeption.Split('=');
                 if (parts.Length != 2)
-                    return false; // Возникает из-за того что есть только левая или только правая часть функции (Ошибка)
+                    return false; 
 
-                var left = parts[0].Trim(); //определение названия и аргрументов функции
+                var left = parts[0].Trim();
 
-                // Проверка на наличие определения функции
+              
                 if (left.Contains("(") && left.Contains(")"))
                 {
                     return true;
                 }
                 else
                 {
-                    return false; // Нет , так как возможно создается переменная 
+                    return false; 
                 }
             }
             else
             {
-                return false; // Нет , так как это выражение для подсчета
+                return false; 
             }
 
         }
 
-        private void isValidArgument(string argument) // Метод проверки валидности символов в названии аргументов
+        private void isValidArgument(string argument) 
         {
             foreach (char character in invalidCharacters)
             {
@@ -83,7 +83,7 @@ namespace Calculator
 
         }
 
-        private void doesBodyContainsArgument(string body, string[] argumnets) // Метод проверка содрежит ли тело функции один из аргументов функции
+        private void doesBodyContainsArgument(string body, string[] argumnets) 
         {
             // Счётчик аргуметов в теле функции
             int counter = 0;
@@ -100,44 +100,41 @@ namespace Calculator
             }
         }
 
-        static string AddMultiplicationSigns(string expression)// Метод добавляет знак * между числом и переменной (3x = 3*x)
+        static string AddMultiplicationSigns(string expression)
         {
-            // Регулярное выражение для поиска числа, за которым следует переменная
+           
             string pattern = @"(\d)([a-zA-Z])";
 
-            // Замена найденных совпадений на число * переменная
             string replacement = "$1*$2";
-
-            // Применение замены к исходному выражению
 
             return Regex.Replace(expression, pattern, replacement);
         }
 
-        public UserFunction CreateUserFunction(string expression) // Метод создания пользовательской функции
+        public UserFunction CreateUserFunction(string expression) 
         {
             UserFunction userFunction = new UserFunction(null, null, null);
 
-            var parts = expression.Split('=');// разделение выражения на части (определение функции и тело функции)
-            var definition = parts[0].Trim(); //определения названия и аргрументов функции
-            var body = AddMultiplicationSigns(parts[1].Trim()); // тело функции
+            var parts = expression.Split('=');
+            var definition = parts[0].Trim(); 
+            var body = AddMultiplicationSigns(parts[1].Trim()); 
 
-            // Найти позицию открывающей круглой скобки в определении функции
+     
             var openParen = definition.IndexOf('(');
-            // Найти позицию закрывающей круглой скобки в определении функции
+           
             var closeParen = definition.IndexOf(')');
 
-            // Извлечь имя функции из строки определения
+            
             string name = definition.Substring(0, openParen).Trim();
-            // Извлечь аргументы функции из строки определения и разбить их по запятым
+          
             var args = definition.Substring(openParen + 1, closeParen - openParen - 1).Split(',');
 
-            // Убрать лишние пробелы у каждого аргумента
+         
             for (int i = 0; i < args.Length; i++)
             {
                 args[i] = args[i].Trim();
-                isValidArgument(args[i]);// Проверка на название аргументов
+                isValidArgument(args[i]);
             }
-            doesBodyContainsArgument(body, args);//Проверка на наличие аргумента в теле функции
+            doesBodyContainsArgument(body, args);
             userFunction.Name = name;
             userFunction.FunctionArgs = args;
             userFunction.Body = "(" + body + ")";
@@ -146,7 +143,7 @@ namespace Calculator
 
 
 
-        public bool isFunctionContains(string expression, List<UserFunction> usersFunctions) //Метод будет проверять содержаться ли в выражении функция 
+        public bool isFunctionContains(string expression, List<UserFunction> usersFunctions) 
         {
             bool isFunctionUsed = false;
             foreach (var userFunction in usersFunctions)
@@ -160,7 +157,7 @@ namespace Calculator
 
         }
 
-        public string ConvertFunctionsToExpression(string expression, List<UserFunction> usersFunctions) // Метод который конвертирует введеную строку с пользовательскими функциями в обычную строку для польской итерации
+        public string ConvertFunctionsToExpression(string expression, List<UserFunction> usersFunctions) 
         {
             while (isFunctionContains(expression, usersFunctions))
             {
@@ -168,14 +165,13 @@ namespace Calculator
                 {
                     if (expression.Contains(userFunction.Name + "("))
                     {
-                        // Найти начало вызова функции
+                        
                         var startIndex = expression.IndexOf(userFunction.Name + "(");
-                        // Найти конец вызова функции (соответствующую закрывающую скобку)
+                     
                         var endIndex = FindMatchingParenthesis(expression, startIndex + userFunction.Name.Length + 1);
-                        // Извлечь строку аргументов функции
+                        
                         var argsString = expression.Substring(startIndex + userFunction.Name.Length + 1, endIndex - startIndex - userFunction.Name.Length - 1);
 
-                        // Разбить аргументы по запятым и вычислить их значения
                         var args = argsString.Split(',');
 
                         var localVars = new Dictionary<string, double>();
@@ -183,19 +179,19 @@ namespace Calculator
                         {
                             localVars[userFunction.FunctionArgs[i]] = Convert.ToDouble(args[i]);
                         }
-                        // Тело функции
+                        
                         string userFunctionBody = userFunction.Body;
-                        // Заменить имена переменных на их значения в выражении
+                       
                         for (int i = 0; i < userFunction.FunctionArgs.Length; i++)
                         {
-                            //Позволяет найти подстроку, которая является отдельным словом ,а не частью другого слова 
-                            string pattern = $@"\b{userFunction.FunctionArgs[i]}\b";// \b - указывает на начало и конец подстроки
+                            
+                            string pattern = $@"\b{userFunction.FunctionArgs[i]}\b";
                             userFunctionBody = Regex.Replace(userFunctionBody, pattern, args[i]);
                         }
 
-                        //Получение текста вызова функции
+                      
                         string functionInitText = userFunction.Name + "(" + argsString + ")";
-                        //Изменение входной строки на формулу функции с числами
+                      
                         expression = expression.Replace(functionInitText, userFunctionBody);
                         break;
                     }
@@ -204,11 +200,10 @@ namespace Calculator
             return expression;
         }
 
-        static int FindMatchingParenthesis(string expression, int startIndex)// Метод поиска нужной закрывающей скобки
+        static int FindMatchingParenthesis(string expression, int startIndex)
         {
             int depth = 0;
 
-            // Пройтись по символам строки, начиная с заданного индекса
             for (int i = startIndex; i < expression.Length; i++)
             {
                 if (expression[i] == '(')
@@ -216,12 +211,11 @@ namespace Calculator
                 else if (expression[i] == ')')
                 {
                     if (depth == 0)
-                        return i;//Соответствующая закрывающая скобка для начальной открывающей скобки
-                    depth--;//Найдена одна из внутренних закрывающих скобок
+                        return i;
+                    depth--;
                 }
             }
-
-            // Если не найдено соответствующей закрывающей скобки, выбросить исключение
+         
             throw new Exception("Не найдено соответствующей закрывающей скобки.");
         }
 
